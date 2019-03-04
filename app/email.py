1a2a1c2f7484b268b29d474e202f654e7b74e959
roxaline <mariadelarosita078@gmail.com>
2019-03-04 19:53:43 +0200

    add all

--- app/email.py
+++ app/email.py
@@ -4,7 +4,6 @@ from . import mail
 
 def mail_message(subject,template,to,**kwargs):
     sender_email = 'mariadelarosita078@gmail.com'
-
     email = Message(subject, sender=sender_email, recipients=[to])
 
     email.body= render_template(template + ".txt",**kwargs)
